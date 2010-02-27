474f355239e91e84cf0443217a2bf0fb41bd9a23
MedeaMelana <MedeaMelana@80548220-2190-11df-970b-617f64f7e5c1>
2010-02-27 16:52:55 +0000

    Added support for INSERT, UPDATE and DELETE queries.

--- Sirenial/BuurtlinkQueries.hs
+++ Sirenial/BuurtlinkQueries.hs
@@ -20,25 +20,35 @@ data AdWeek = AdWeek
   }
 
 selectAds :: (TableAlias Db.Ad -> Expr Bool) -> ExecSelect [Ad]
-selectAds f = do
+selectAds p = do
   -- Query Db.tableAd
   ads <- execSelect $ do
     a <- from Db.tableAd
-    restrict (f a)
+    restrict (p a)
     return $ Ad <$> a # Db.adId <*> a # Db.adStatus <*> pure []
 
-  -- Per ad, query Db.tableAdWeek
+  -- Per ad, query Db.tableAdWeek to fill the adWeeks field
   for ads $ \ad -> do
-    weeks <- execSelect $ do
-      aw <- from Db.tableAdWeek
-      restrict $ aw # Db.adWeekAdId `ExEq` ExRef (adId ad)
-      return $ AdWeek <$> aw # Db.adWeekStartsOn <*> aw # Db.adWeekTownId
-    return (ad { adWeeks = weeks })
+    if adStatus ad == "confirmed"
+      then do
+        weeks <- execSelect $ do
+          aw <- from Db.tableAdWeek
+          restrict $ aw # Db.adWeekAdId .==. expr (adId ad)
+          return $ AdWeek <$> aw # Db.adWeekStartsOn <*> aw # Db.adWeekTownId
+        return (ad { adWeeks = weeks })
+      else do
+        return ad
 
 selectAllAds :: ExecSelect [Ad]
-selectAllAds = selectAds (\_ -> ExBool True)
+selectAllAds = selectAds (\_ -> expr True)
 
 selectAdById :: Ref Db.Ad -> ExecSelect Ad
 selectAdById adId = do
-  [ad] <- selectAds (\a -> a # Db.adId `ExEq` ExRef adId)
+  [ad] <- selectAds (\a -> a # Db.adId .==. expr adId)
   return ad
+
+setAdStatus :: Ref Db.Ad -> String -> ModifyStmt
+setAdStatus adId newStatus =
+  ExecUpdate Db.tableAd $ \a ->
+    ( [Db.adStatus := expr newStatus]
+    , a # Db.adId .==. expr adId )
--- Sirenial/Sirenial/Query.hs
+++ Sirenial/Sirenial/Query.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Sirenial.Query (
     -- * Describing tables
@@ -8,8 +9,8 @@ module Sirenial.Query (
     primKey, foreignKey,
 
     -- * SQL expressions
-    Expr(..), TableAlias(..),
-    (#),
+    Expr(..), TableAlias(..), ToExpr(..),
+    (#), (.==.),
 
     -- * Building SELECT queries
     Select(..),
@@ -18,12 +19,16 @@ module Sirenial.Query (
     -- * Executing SELECT queries
     ExecSelect(..), Merge(..),
     execSelect, for,
+    
+    -- * INSERT, UPDATE and DELETE queries
+    ModifyStmt(..), SetField(..),
   ) where
 
 import qualified Data.Traversable as T
 import Data.Time.Calendar
 import Control.Applicative
 import Control.Monad
+import Text.Read
 
 
 -- Describing tables and their fields.
@@ -36,6 +41,10 @@ data Table t = Table
 
 -- | @Ref t@ is the type of the primary key to table @t@.
 newtype Ref t = Ref { getRef :: Int }
+  deriving Eq
+
+instance Show (Ref t) where show (Ref r) = show r
+instance Read (Ref t) where readPrec = Ref <$> readPrec
 
 -- | A typed field in a table.
 data Field t a = Field
@@ -63,13 +72,14 @@ foreignKey t name _ = Field t name TyRef
 
 -- | SQL expressions indexed by their type.
 data Expr a where
-  ExPure   :: a -> Expr a
-  ExApply  :: Expr (a -> b) -> Expr a -> Expr b
-  ExGet    :: TableAlias t -> Field t a -> Expr a
-  ExEq     :: Expr a -> Expr a -> Expr Bool
-  ExBool   :: Bool -> Expr Bool
-  ExRef    :: Ref a -> Expr (Ref a)
-  ExAlias  :: Int -> Expr (TableAlias t)
+  ExPure    :: a -> Expr a
+  ExApply   :: Expr (a -> b) -> Expr a -> Expr b
+  ExGet     :: TableAlias t -> Field t a -> Expr a
+  ExEq      :: Eq a => Expr a -> Expr a -> Expr Bool
+  ExBool    :: Bool -> Expr Bool
+  ExString  :: String -> Expr String
+  ExRef     :: Ref t -> Expr (Ref t)
+  ExAlias   :: Int -> Expr (TableAlias t)
 
 instance Functor Expr where
   fmap   = liftA
@@ -81,9 +91,20 @@ instance Applicative Expr where
 -- | SQL constructs such as FROM and JOIN introduce new table aliases.
 data TableAlias t = TableAlias { getAlias :: Int }
 
+-- | Lift a value into the 'Expr' functor.
+class     ToExpr a        where expr :: a -> Expr a
+instance  ToExpr [Char]   where expr = ExString
+instance  ToExpr Bool     where expr = ExBool
+instance  ToExpr (Ref t)  where expr = ExRef
+
 -- | Retrieve a field from a table. (An alias for 'ExGet'.)
 (#) :: TableAlias t -> Field t a -> Expr a
 (#) = ExGet
+
+-- | Compare two values for equality.
+(.==.) :: Eq a => Expr a -> Expr a -> Expr Bool
+(.==.) = ExEq
+infixl 4 .==.
 
 
 -- Building SELECT queries
@@ -151,3 +172,16 @@ execSelect = EsExec
 -- | Run queries for each element in a list. The queries may be merged into optimized queries.
 for :: [a] -> (a -> ExecSelect b) -> ExecSelect [b]
 for xs f = EsExecMany (T.for xs (MeSelect . f))
+
+
+-- UPDATE, INSERT and DELETE statements.
+
+-- | An SQL modification statement.
+data ModifyStmt where
+  ExecUpdate  :: Table t -> (TableAlias t -> ([SetField t], Expr Bool))  -> ModifyStmt
+  ExecInsert  :: Table t -> (TableAlias t ->  [SetField t]            )  -> ModifyStmt
+  ExecDelete  :: Table t -> (TableAlias t ->                Expr Bool )  -> ModifyStmt
+
+-- | Set a table field to a particular value.
+data SetField t where
+  (:=) :: Field t a -> Expr a -> SetField t
