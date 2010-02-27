9b2bfafbc21d87243d7d28f5c488c3cf6b09eb94
MedeaMelana <MedeaMelana@80548220-2190-11df-970b-617f64f7e5c1>
2010-02-27 11:57:10 +0000

    Added Makefile. Merged Merge and Query.

--- Sirenial/Sirenial/Query.hs
+++ Sirenial/Sirenial/Query.hs
@@ -5,21 +5,30 @@
 module Sirenial.Query (
     -- * Describing tables
     Table(..), Ref(..), Field(..), FieldType(..),
-    TableAlias(..), Expr(..),
-    
-    -- * SELECT queries
-    Select(..), JoinType(..), SelectStmt(..), ExecSelect,
-    primKey, foreignKey, (#), from, leftJoin, returnAll,
+    primKey, foreignKey,
+
+    -- * SQL expressions
+    Expr(..), TableAlias(..),
+    (#),
+
+    -- * Building SELECT queries
+    Select(..), JoinType(..), SelectStmt(..),
+    from, leftJoin, returnAll,
+
+    -- * Executing SELECT queries
+    ExecSelect(..), Merge(..),
+    for,
   ) where
 
+import Data.Traversable (sequenceA)
+import Data.Time.Calendar
 import Control.Applicative
 import Control.Monad
-import Data.Time.Calendar
 
 
--- Types
+-- Describing tables and their fields.
 
--- | A table description, indexed by its own (phantom) index.
+-- | A table description, indexed by its own phantom type.
 data Table t = Table
   { tableName  :: String
   , tableKey   :: Field t (Ref t)
@@ -40,8 +49,16 @@ data FieldType :: * -> * where
   TyInt     :: FieldType Int
   TyString  :: FieldType String
   TyDay     :: FieldType Day
-  TyRef     :: FieldType (Ref a)
+  TyRef     :: FieldType (Ref t)
   TyMaybe   :: FieldType a -> FieldType (Maybe a)
+
+-- | Describe a primary key field named @\"id\"@.
+primKey :: Table t -> Field t (Ref t)
+primKey t = Field t "id" TyRef
+
+-- | Describe a foreign key from table @t@ to table @t'@.
+foreignKey :: Table t -> String -> Table t' -> Field t (Ref t')
+foreignKey t name _ = Field t name TyRef
 
 
 -- | SQL expressions indexed by their type.
@@ -50,8 +67,9 @@ data Expr a where
   ExApply  :: Expr (a -> b) -> Expr a -> Expr b
   ExGet    :: TableAlias t -> Field t a -> Expr a
   ExEq     :: Expr a -> Expr a -> Expr Bool
-  ExTrue   :: Expr Bool
+  ExBool   :: Bool -> Expr Bool
   ExRef    :: Int -> Expr (Ref a)
+  ExAlias  :: Int -> Expr (TableAlias t)
 
 instance Functor Expr where
   fmap   = liftA
@@ -60,12 +78,15 @@ instance Applicative Expr where
   pure   = ExPure
   (<*>)  = ExApply
 
-
 -- | SQL constructs such as FROM and JOIN introduce new table aliases.
 data TableAlias t = TableAlias { getAlias :: Int }
 
+-- | Retrieve a field from a table. (An alias for 'ExGet'.)
+(#) :: TableAlias t -> Field t a -> Expr a
+(#) = ExGet
 
--- SELECT
+
+-- Building SELECT queries
 
 -- | The Select monad handles the supply of new table aliases arising from FROMs and JOINs.
 data Select a where
@@ -73,11 +94,6 @@ data Select a where
   SeBind    :: Select a -> (a -> Select b) -> Select b
   SeFrom    :: Table t -> Select (TableAlias t)
   SeJoin    :: JoinType -> Field t a -> Expr a -> Select (TableAlias t)
-
-data SelectStmt a = SelectStmt
-  { ssResult :: Expr a
-  , ssWhere  :: Expr Bool
-  }
 
 instance Functor Select where
   fmap    = liftM
@@ -89,32 +105,57 @@ instance Monad Select where
 data JoinType = InnerJoin | LeftJoin | RightJoin
   deriving (Eq, Read, Show, Enum)
 
--- | Execute a SELECT statement.
-type ExecSelect = forall a. Select (SelectStmt a) -> IO [a]
-
-
--- SELECT aliases
+data SelectStmt a = SelectStmt
+  { ssResult :: Expr a
+  , ssWhere  :: Expr Bool
+  }
 
 -- | Select from a table. (An alias for 'SeFrom'.)
 from :: Table t -> Select (TableAlias t)
 from = SeFrom
 
--- | Retrieve a field from a table. (An alias for 'ExGet'.)
-(#) :: TableAlias t -> Field t a -> Expr a
-(#) = ExGet
-
 -- | Short for @'SeJoin' 'LeftJoin'@.
 leftJoin :: Field t a -> Expr a -> Select (TableAlias t)
 leftJoin = SeJoin LeftJoin
 
--- | Describe a primary key field named @"id"@.
-primKey :: Table t -> Field t (Ref t)
-primKey t = Field t "id" TyRef
-
--- | Describe a foreign key from table @t@ to table @t'@.
-foreignKey :: Table t -> String -> Table t' -> Field t (Ref t')
-foreignKey t name _ = Field t name TyRef
-
 -- | Return an expression with no WHERE clause.
 returnAll :: Expr a -> Select (SelectStmt a)
-returnAll expr = return (SelectStmt expr ExTrue)
+returnAll expr = return (SelectStmt expr (ExBool True))
+
+
+-- Executing SELECT queries
+
+-- | Monadic context in which SELECT queries may be executed.
+data ExecSelect a where
+  EsReturn    :: a -> ExecSelect a
+  EsBind      :: ExecSelect a -> (a -> ExecSelect b) -> ExecSelect b
+  EsExec      :: Select (SelectStmt a) -> ExecSelect [a]
+  EsExecMany  :: Merge a -> ExecSelect a
+
+instance Functor ExecSelect where
+  fmap    = liftM
+
+instance Applicative ExecSelect where
+  pure    = return
+  (<*>)   = ap
+
+instance Monad ExecSelect where
+  return  = EsReturn
+  (>>=)   = EsBind
+
+-- | Applicative context in which 'ExecSelect's may be merged into optimized queries.
+data Merge a where
+  MePure    :: a -> Merge a
+  MeApply   :: Merge (a -> b) -> Merge a -> Merge b
+  MeSelect  :: ExecSelect a -> Merge a
+
+instance Functor Merge where
+  fmap   = liftA
+
+instance Applicative Merge where
+  pure   = MePure
+  (<*>)  = MeApply
+
+-- | Run queries for each element in a list. The queries may be merged into optimized queries.
+for :: [a] -> (a -> ExecSelect b) -> ExecSelect [b]
+for xs f = (EsExecMany . sequenceA . map (MeSelect . f)) xs
