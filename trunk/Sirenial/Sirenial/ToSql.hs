100452843f2f91c1acbb3c9452dda4e8bda27166
MedeaMelana <MedeaMelana@80548220-2190-11df-970b-617f64f7e5c1>
2010-03-02 19:30:57 +0000

    Merging works for the most part now.

--- trunk/Sirenial/Sirenial/ToSql.hs
+++ trunk/Sirenial/Sirenial/ToSql.hs
@@ -6,11 +6,15 @@ import Sirenial.Tables
 import Sirenial.Expr
 import Sirenial.Select
 
+import Data.Function
 import Data.List
+import qualified Data.Traversable as T
 import Control.Applicative
 import Control.Monad.Writer
+import Control.Concurrent.MVar
 
 import Database.HDBC
+import Debug.Trace
 
 
 selectedFields :: Expr a -> [(Int, String)]
@@ -129,3 +133,127 @@ combine (SelectStmt ts1 c1 r1) (SelectStmt ts2 c2 r2)
              , [y | (_, _   , y, True) <- rows]
              )
   | otherwise   = Nothing
+
+data Suspend a where
+  SuPure    :: a -> Suspend a
+  SuApply   :: Suspend (a -> b) -> Suspend a -> Suspend b
+  SuSelect  :: SelectStmt a -> Maybe (MVar [a]) -> ([a] -> Suspend b) -> Suspend b
+
+instance Functor Suspend where
+  fmap     = liftA
+
+instance Applicative Suspend where
+  pure     = SuPure
+  (<*>)    = SuApply
+
+instance Monad Suspend where
+  return   = SuPure
+  s >>= f  =
+    case s of
+      SuPure x -> f x
+      SuApply sg sx -> do
+        g <- sg
+        x <- sx
+        f (g x)
+      SuSelect s v g -> SuSelect s v (\xs -> g xs >>= f)
+
+suspend :: Merge a -> Suspend a
+suspend m =
+  case m of
+    MePure x -> SuPure x
+    MeApply f x -> SuApply (suspend f) (suspend x)
+    MeSelect es ->
+      let s = suspendES es
+        -- in trace ("Suspending ES. Resulting Suspend size: " ++ show (size s)) s
+        in s
+
+suspendES :: ExecSelect a -> Suspend a
+suspendES es =
+  case es of
+    EsReturn x -> SuPure x
+    EsExecMany m -> suspend m -- trace ("Suspending Merge with size " ++ show (sizeMe m)) $ suspend m
+    EsExec s -> SuSelect s Nothing SuPure
+    EsBind (EsReturn x) f -> suspendES (f x)
+    EsBind (EsExec s) f -> SuSelect s Nothing (suspendES . f)
+    EsBind (EsExecMany m) f -> suspend m >>= suspendES . f
+    EsBind es' f -> suspendES es' >>= suspendES . f
+
+data Pendulum where
+  Pendulum :: SelectStmt a -> MVar [a] -> Pendulum
+
+newtype CollectResult a = CollectResult (Either a (Suspend a, [Pendulum]))
+
+instance Functor CollectResult where
+  fmap f (CollectResult res) =
+    CollectResult $ case res of
+      Left x -> Left (f x)
+      Right (s, ps) -> Right (fmap f s, ps)
+
+collect :: Suspend a -> IO (CollectResult a)
+collect s =
+  case s of
+    SuPure x -> return (CollectResult (Left x))
+    SuApply sf sx -> do
+      CollectResult cf <- collect sf
+      CollectResult cx <- collect sx
+      case (cf, cx) of
+        (Left f, _) ->
+          return (f <$> CollectResult cx)
+        (Right _, Left x) ->
+          return (($ x) <$> CollectResult cf)
+        (Right (sf', fps), Right (sx', xps)) ->
+          return $ CollectResult $ Right (sf' <*> sx', fps ++ xps)
+    SuSelect s mv f -> do
+      v <- case mv of
+        Nothing  -> newEmptyMVar
+        Just v   -> return v
+      mrs <- tryTakeMVar v
+      case mrs of
+        Nothing ->
+          return $ CollectResult $ Right $ (SuSelect s (Just v) f, [Pendulum s v])
+        Just rs ->
+          collect (f rs)
+
+progress :: IConnection conn => conn -> [Pendulum] -> IO ()
+progress conn (p:ps') = do
+    putStrLn $ "*** Merging " ++ show (length ps) ++ " queries. "
+      ++ show (1 + length ps' - length ps) ++ " queries still pending."
+    T.for ps $ \(Pendulum _ v) -> putMVar v []
+    let stmt = SelectStmt
+          { ssFroms   = froms p
+          , ssCrit    = exprOr (map crit ps)
+          , ssResult  = T.for ps $ \(Pendulum s v) -> update v <$> ssCrit s <*> ssResult s
+          }
+    actions <- execSingle conn stmt
+    sequence_ (concat actions)
+  where
+    ps = p : filter ok ps'
+    ok p' = froms p == froms p'
+    froms (Pendulum s _) = ssFroms s
+    crit (Pendulum s _) = ssCrit s
+    update v b r =
+      when b $ modifyMVar_ v (\rs -> return (rs ++ [r]))
+
+runSuspend :: IConnection conn => conn -> Suspend a -> IO a
+runSuspend conn s = do
+  putStrLn $ "*** Running Suspend of size " ++ show (size s)
+  CollectResult res <- collect s
+  case res of
+    Left x -> return x
+    Right (s', ps) -> do
+      progress conn ps
+      runSuspend conn s'
+
+size :: Suspend a -> Int
+size s =
+  case s of
+    SuPure _ -> 0
+    SuApply f x -> size f + size x
+    SuSelect _ _ _ -> 1
+
+sizeMe :: Merge a -> Int
+sizeMe m =
+  case m of
+    MePure _ -> 0
+    MeApply f x -> sizeMe f + sizeMe x
+    MeSelect _ -> 1
